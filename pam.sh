27418716fc426e6e527ea5544e368a2d6c96a59f
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-12-15 12:20:09 +0330

    Made basic validation of parameters.

--- pam.sh
+++ pam.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
-echo "HELLO WORLD"
+# VARIABLES
+REPO_BRANCHES=$(git branch)
+REPO_BRANCHES=${REPO_BRANCHES/\*/}
+# REPO_BRANCHES=${REPO_BRANCHES//\\n/}
+REPO_REMOTES=$(git remote -v)
+REMOTE=${3:-origin}
+
+if [ ! -d ".git" ]; then
+  echo "ERROR: This is not a git repo. Please run the command in a git repository."
+  exit 1
+fi
+
+if [[ -z $1 || -z $2 ]]; then
+  echo "ERROR: You have to provide two branches as parameters. See \"pam -h\""
+  exit 1
+fi
+
+if [[ -z $3 ]]; then
+  echo "No remote has been provided. Default remote is \"origin\""
+fi
+
+if [[ ! "$REPO_BRANCHES" == *"$1"* ]]; then
+  echo "ERROR: There is no branch called \"$1\""
+  exit 1
+fi
+
+if [[ ! "$REPO_BRANCHES" == *"$2"* ]]; then
+  echo "ERROR: There is no branch called \"$2\""
+  exit 1
+fi
+
+if [[ ! "$REPO_REMOTES" == *"$REMOTE"* ]]; then
+  echo "ERROR: There is no remote called \"$REMOTE\""
+  exit 1
+fi
