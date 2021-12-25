db75d26ff5ca3fd20b069f76f76acc2dfbe14b73
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-12-25 17:39:53 +0330

    fix : put script in /usr/local/bin

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ echo "Getting source code from repo..."
 wget -q -O - https://github.com/MahdiAghaei1/pam/releases/download/v0.0.1/pam.sh > temp.sh
 
 echo "Making pam executable..."
-cp temp.sh /usr/bin/pam &&
-chmod +rx /usr/bin/pam &&
+cp temp.sh /usr/local/bin/pam &&
+chmod +rx /usr/local/bin/pam &&
 rm temp.sh
 
 echo "pam is now installed. enjoy"
