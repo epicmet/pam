c85a8801aa7d7aeebed3cc4dafe0529d3034df70
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2021-12-28 23:53:15 +0330

    fix: prevent installing empty script when there is an error on downloading the source code

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 echo "Getting source code from repo..."
 wget -q -O - https://raw.githubusercontent.com/MahdiAghaei1/pam/master/pam.sh > temp.sh
 
+if [ $? -ne 0 ]
+then
+    echo "Error while downloading the source code. Make sure that you have wget installed and your network connection is stable."
+    exit 1
+fi
+
 echo "Making pam executable..."
 sudo cp temp.sh /usr/local/bin/pam &&
 sudo chmod +rx /usr/local/bin/pam &&
